c51c60ee844414576701c8dfda2297e8d2a2aa40
Prabesh Chhetri <prabesh289@gmail.com>
2016-10-08 19:03:15 +0000

    Update Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,20 @@ class PostsController < ApplicationController
         @post = Post.new
     end
     
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        if @post.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
     def create
     @post = current_user.posts.create(post_params)
         if @post.valid?
