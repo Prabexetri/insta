609a72f3c88e7a11db3e64c7e24695cdcb0a5197
Prabesh Chhetri <prabesh289@gmail.com>
2016-10-08 14:55:52 +0000

    Create models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006183709) do
+ActiveRecord::Schema.define(version: 20161008140845) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "photo"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
